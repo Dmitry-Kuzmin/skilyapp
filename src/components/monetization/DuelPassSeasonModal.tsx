960b1ab6f457147503aab567b6c4071a2c83ea5c
Дмитрий Кузьмин <dimka@MacBook-Air-Dmitrij.local>
2025-11-19 23:28:11 +0100

    fix: improve flex structure for onboarding content to fill modal height

--- src/components/monetization/DuelPassSeasonModal.tsx
+++ src/components/monetization/DuelPassSeasonModal.tsx
@@ -1920,8 +1920,10 @@ export function DuelPassSeasonModal({ open, onOpenChange }: { open: boolean; onO
           <DialogContent className="w-[95vw] max-w-5xl max-h-[85vh] overflow-hidden flex flex-col p-0">
             <DialogTitle className="sr-only">Duel Pass Season</DialogTitle>
             <DialogDescription className="sr-only">Duel Pass Season information and rewards</DialogDescription>
-            <div className="flex-1 overflow-y-auto min-h-0">
-              <ModalContent />
+            <div className="flex-1 overflow-hidden min-h-0 flex flex-col">
+              <div className="flex-1 overflow-y-auto min-h-0">
+                <ModalContent />
+              </div>
             </div>
           </DialogContent>
         </Dialog>
