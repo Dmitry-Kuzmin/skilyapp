9e95aff382f804b6cdd6c6ee27b4458c3fd99206
Dmitry-Kuzmin <kuzmin.public@gmail.com>
2026-01-24 10:51:39 +0100

    Debug: Comment out EncryptionFlashlight logic to fix syntax error

--- src/components/duel/DuelQuestionCard.tsx
+++ src/components/duel/DuelQuestionCard.tsx
@@ -170,6 +170,7 @@ const cardContent = (
 );
 
 // Если cryptolocker активен - оборачиваем весь контент в один EncryptionFlashlight
+/* 
 if (cryptolockerActive && !isAnswered) {
   const radius = typeof window !== 'undefined' && window.innerWidth < 768 ? 140 : 180;
   return (
@@ -180,6 +181,7 @@ if (cryptolockerActive && !isAnswered) {
     </Suspense>
   );
 }
+*/
 
 return cardContent;
 });
