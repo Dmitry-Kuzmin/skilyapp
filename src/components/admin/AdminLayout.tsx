04ad47e9d9c1b2c9fc4459ca1fe3f8b0de7e36e7
Dmitry-Kuzmin <kuzmin.public@gmail.com>
2026-03-31 19:19:13 +0200

    auto: 2026-03-31 19:19

--- src/components/admin/AdminLayout.tsx
+++ src/components/admin/AdminLayout.tsx
@@ -17,6 +17,7 @@ import {
   Flag,
   ToggleLeft,
   Rocket,
+  CircleDollarSign,
 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
