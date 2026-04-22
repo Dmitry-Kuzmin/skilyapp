8b03a6a664e6181848edb18c7076cf8d26e0e23d
Dmitry-Kuzmin <kuzmin.public@gmail.com>
2026-04-22 13:16:47 +0200

    auto: 2026-04-22 13:16

--- src/pages/games/FlashCardsGame.tsx
+++ src/pages/games/FlashCardsGame.tsx
@@ -804,7 +804,6 @@ const FlashCardsGame = () => {
               key={currentCardIndex}
               initial={{ opacity: 0, scale: 0.95 }}
               animate={{ opacity: 1, scale: 1 }}
-              exit={{ opacity: 0, scale: 0.95 }}
               transition={{ duration: 0.3 }}
               className="relative"
               onTouchStart={handleTouchStart}
