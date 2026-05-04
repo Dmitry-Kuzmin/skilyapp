1459b8b1d6cf4cf0c158b12b071d9c6b14b0482b
Dmitry-Kuzmin <kuzmin.public@gmail.com>
2026-05-04 12:55:14 +0200

    auto: 2026-05-04 12:55

--- src/pages/Tests.tsx
+++ src/pages/Tests.tsx
@@ -23,7 +23,7 @@ import { motion } from "@/components/optimized/Motion";
 import { getImageUrl } from "@/utils/imageUtils";
 import { loadTestProgress } from "@/utils/testStorage";
 import { cn } from "@/lib/utils";
-import { useModalStore, ModalType } from "@/store/modalStore";
+import { useModalStore } from "@/store/modalStore";
 import { FREE_QUESTION_LIMIT } from "@/lib/premiumState";
 import { AIInsightsLibrary } from "@/components/test-results/AIInsightsLibrary";
 import { useDashboardData } from "@/hooks/useDashboardData";
