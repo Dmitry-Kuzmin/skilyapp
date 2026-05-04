9b75ec1acc5541e46999138f06b39f9b615b21d0
Dmitry-Kuzmin <kuzmin.public@gmail.com>
2026-05-04 21:20:49 +0200

    auto: 2026-05-04 21:20

--- src/components/skily-ai/index.ts
+++ src/components/skily-ai/index.ts
@@ -2,9 +2,7 @@ export { SkilyAICharacter } from './SkilyAICharacter';
 export type { SkilyAIMood } from './SkilyAICharacter';
 export { SkilyAIToast } from './SkilyAIToast';
 export type { SkilyAIToastProps } from './SkilyAIToast';
-export { SkilyAIChatWidget } from './SkilyAIChatWidget';
 export { SkilyAIMessage } from './SkilyAIMessage';
 export { SkilyAISearchWidget } from './SkilyAISearchWidget';
-export { SkilyAIMobileSheet } from './SkilyAIMobileSheet';
 
 
