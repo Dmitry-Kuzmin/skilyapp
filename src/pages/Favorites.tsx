05b90038d025eb8350db822e1fd4e26fa889fcc3
Dmitry-Kuzmin <kuzmin.public@gmail.com>
2026-05-04 16:20:02 +0200

    auto: 2026-05-04 16:20

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -20,7 +20,7 @@ import { motion, AnimatePresence } from "framer-motion";
 import { usePremium } from "@/hooks/usePremium";
 import { useModalStore } from "@/store/modalStore";
 
-const FREE_CARD_LIMIT = 10;
+const FREE_CARD_LIMIT = 8;
 
 // --- TYPES ---
 type FavoriteQuestion = {
