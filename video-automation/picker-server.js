d7506ceb42f602ea47be28824559ac2276a6fa6d
Dmitry-Kuzmin <kuzmin.public@gmail.com>
2026-05-05 16:25:36 +0200

    auto: 2026-05-05 16:25

--- video-automation/picker-server.js
+++ video-automation/picker-server.js
@@ -1366,7 +1366,7 @@ function updatePublishPanel() {
     html += '<div class="publish-file-row">'
       + '<span class="publish-file-lang">ES</span>'
       + '<span class="publish-file-name" title="' + r.output + '">' + name + '</span>'
-      + '<button class="btn-finder" onclick="revealFile(\'' + r.output.replace(/\\/g,'\\\\').replace(/'/g,"\\'") + '\')">📂 Finder</button>'
+      + '<button class="btn-finder" data-path="' + r.output.replace(/"/g, '&quot;') + '" onclick="revealFile(this.dataset.path)">📂 Finder</button>'
       + '</div>';
   }
   if (r.outputRU) {
