feb3da40c5d50c6e7e8e5b3b9e6aa924d01039ea
Dmitry-Kuzmin <kuzmin.public@gmail.com>
2026-05-07 21:22:53 +0200

    auto: 2026-05-07 21:22

--- src/components/celebration/TestResultsCelebrationFlow.tsx
+++ src/components/celebration/TestResultsCelebrationFlow.tsx
@@ -744,7 +744,7 @@ export function TestResultsCelebrationFlow({ data, onDone }: Props) {
 
   const slides: SlideId[] = [
     'result',
-    ...(data.spAwarded > 0 ? ['sp' as SlideId] : []),
+    'sp',
     ...(data.timeSeconds > 0 ? ['time' as SlideId] : []),
     'xp',
     ...(data.failedTopics.length > 0 ? ['topics' as SlideId] : []),
