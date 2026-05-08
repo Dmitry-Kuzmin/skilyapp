74284ddad6354935957a985cf368e21b055675dd
Dmitry-Kuzmin <kuzmin.public@gmail.com>
2026-05-08 18:00:34 +0200

    auto: 2026-05-08 18:00

--- sdadim-eu-astro/astro.config.mjs
+++ sdadim-eu-astro/astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import react from '@astrojs/react';
 
 export default defineConfig({
   site: 'https://sdadim.eu',
   output: 'static',
   integrations: [
+    react(),
     tailwind(),
     sitemap({
       changefreq: 'weekly',
